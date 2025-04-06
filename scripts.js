86c794f812e559418735aef46ab76ad38d179c84
ChrisRoland <chrisebuberoland@gmail.com>
2025-04-06 11:37:45 +0100

    update

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const snap = document.querySelector('.shuttersound');
 
 function getVideo() {
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
